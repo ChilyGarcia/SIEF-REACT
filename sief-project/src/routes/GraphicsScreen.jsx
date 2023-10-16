a561cdb8b3c89d0c8560e7dbc37e1adf89d58ffa
juan <jugada160324@gmail.com>
2023-10-16 14:21:43 -0500

    Se hace modificaciones en el componente del panel para abrir o cerrar el sidebar con el boton de menu, tambien se pone un nav para poner el usaurio y gestionar si abrir o cerrar el sidebar

--- sief-project/src/routes/GraphicsScreen.jsx
+++ sief-project/src/routes/GraphicsScreen.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { PanelComponent } from "./components/PanelComponent";
 import { GraphicsCompontent } from "./components/GraphicsCompontent";
+import styles from "../styles/graphicsStyles.module.css";
 
 export const GraphicsScreen = () => {
   return (
     <>
       <PanelComponent></PanelComponent>
+      <div className={styles.body}></div>
       <GraphicsCompontent></GraphicsCompontent>
     </>
   );
--- sief-project/src/routes/components/PanelComponent.jsx
+++ sief-project/src/routes/components/PanelComponent.jsx
@@ -1,77 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/panelStyles.module.css";
 import { Link } from "react-router-dom";
 
 export const PanelComponent = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <>
-      <div className={styles.area}></div>
-      <nav className={`${styles["main-menu"]} ${styles["fixed-menu"]}`}>
-        <ul>
-          <li>
-            <Link to="/graficas">
-              <i
-                className={`fa fa-bar-chart-o fa-2x ${styles["fa"]} ${styles["fa-2x"]}`}
-              ></i>
-              <span className={styles["nav-text"]}>Gráficas</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/infoEstadistica">
-              <i
-                className={`fa fa-book fa-2x ${styles["fa"]} ${styles["fa-2x"]}`}
-              ></i>
+    <div className={styles.panelContainer}>
+      <div
+        className={`${styles.navbar} ${
+          isSidebarOpen ? styles["navbar-open"] : ""
+        }`}
+      >
+        <button onClick={toggleSidebar}>
+          <i className="fa fa-bars"></i>
+        </button>
+      </div>
+      <div
+        className={`${styles.sidebar} ${
+          isSidebarOpen ? styles["sidebar-open"] : ""
+        }`}
+      >
+        <div className={styles["sidebar-content"]}>
+          <nav className={`${styles["main-menu"]} ${styles["sidebar-open"]}`}>
+            <ul>
+              <li>
+                <Link to="/graficas" className={styles["round-corners"]}>
+                  <i
+                    className={`fa fa-bar-chart-o fa-2x ${styles["fa"]} ${styles["fa-2x"]}`}
+                  ></i>
+                  <span className={styles["nav-text"]}>Gráficas</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/infoEstadistica" className={styles["round-corners"]}>
+                  <i
+                    className={`fa fa-book fa-2x ${styles["fa"]} ${styles["fa-2x"]}`}
+                  ></i>
+                  <span className={styles["nav-text"]}>
+                    Información estadística
+                  </span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/auditorias" className={styles["round-corners"]}>
+                  <i
+                    className={`fa fa-table fa-2x ${styles["fa"]} ${styles["fa-2x"]}`}
+                  ></i>
+                  <span className={styles["nav-text"]}>Auditorias</span>
+                </Link>
+              </li>
+              <li className={styles["has-subnav"]}>
+                <Link to="/nuevaInfo" className={styles["round-corners"]}>
+                  <i
+                    className={`fa fa-plus fa-2x ${styles["fa"]} ${styles["fa-2x"]}`}
+                  ></i>
+                  <span className={styles["nav-text"]}>Nueva información</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/infoHistorica" className={styles["round-corners"]}>
+                  <i
+                    className={`fa fa-info fa-2x ${styles["fa"]} ${styles["fa-2x"]}`}
+                  ></i>
+                  <span className={styles["nav-text"]}>
+                    Informacion histórica
+                  </span>
+                </Link>
+              </li>
+              <li className={styles["has-subnav"]}>
+                <Link to="/nuevoUsuario" className={styles["round-corners"]}>
+                  <i
+                    className={`fa fa-user fa-2x ${styles["fa"]} ${styles["fa-2x"]}`}
+                  ></i>
+                  <span className={styles["nav-text"]}>Nuevo usuario</span>
+                </Link>
+              </li>
+            </ul>
 
-              <span className={styles["nav-text"]}>
-                Información estadistica
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/auditorias">
-              <i
-                className={`fa fa-table fa-2x ${styles["fa"]} ${styles["fa-2x"]}`}
-              ></i>
-              <span className={styles["nav-text"]}>Auditorias</span>
-            </Link>
-          </li>
-          <li className={styles["has-subnav"]}>
-            <Link to="/nuevaInfo">
-              <i
-                className={`fa fa-plus fa-2x ${styles["fa"]} ${styles["fa-2x"]}`}
-              ></i>
-              <span className={styles["nav-text"]}>Nueva información</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/infoHistorica">
-              <i
-                className={`fa fa-info fa-2x ${styles["fa"]} ${styles["fa-2x"]}`}
-              ></i>
-              <span className={styles["nav-text"]}>Informacion histórica</span>
-            </Link>
-          </li>
-          <li className={styles["has-subnav"]}>
-            <Link to="/nuevoUsuario">
-              <i
-                className={`fa fa-user fa-2x ${styles["fa"]} ${styles["fa-2x"]}`}
-              ></i>
-              <span className={styles["nav-text"]}>Nuevo usuario</span>
-            </Link>
-          </li>
-        </ul>
-
-        <ul className={styles.logout}>
-          <li>
-            <Link to="/login">
-              <i
-                className={`fa fa-power-off fa-2x ${styles["fa"]} ${styles["fa-2x"]}`}
-              ></i>
-              <span className={styles["nav-text"]}>Cerrar sesión</span>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </>
+            <ul className={styles.logout}>
+              <li>
+                <Link to="/login" className={styles["round-corners"]}>
+                  <i
+                    className={`fa fa-power-off fa-2x ${styles["fa"]} ${styles["fa-2x"]}`}
+                  ></i>
+                  <span className={styles["nav-text"]}>Cerrar sesión</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
   );
 };
