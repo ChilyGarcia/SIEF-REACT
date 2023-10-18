c30ae62873410ac0d60a10627136e88f0bd2be64
juan <jugada160324@gmail.com>
2023-10-18 18:03:31 -0500

    Se hace actualizaciones al componente de informacion estadistica, donde se aggrega la paginacion a la tabla de la informacion estadistica

--- sief-project/src/routes/components/StatisticalInformationComponent.jsx
+++ sief-project/src/routes/components/StatisticalInformationComponent.jsx
@@ -1,12 +1,156 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../../styles/statisticalInformationStyles.module.css";
 
 export const StatisticalInformationComponent = ({ estadoSideBar }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
+  // Datos simulados (ajusta estos datos según tus necesidades)
+  const data = [
+    {
+      Código: "27001",
+      Programa: "Ingenieria de software",
+      Periodo: "2023",
+      Inscritos: 100,
+      Admitidos: 80,
+      Matriculados: 70,
+      Graduados: 60,
+    },
+    {
+      Código: "27001",
+      Programa: "Ingenieria de software",
+      Periodo: "2023",
+      Inscritos: 90,
+      Admitidos: 75,
+      Matriculados: 65,
+      Graduados: 55,
+    },
+    {
+      Código: "27001",
+      Programa: "Ingenieria de software",
+      Periodo: "2023",
+      Inscritos: 90,
+      Admitidos: 75,
+      Matriculados: 65,
+      Graduados: 55,
+    },
+    {
+      Código: "10902",
+      Programa: "Diseño grafico",
+      Periodo: "2023",
+      Inscritos: 90,
+      Admitidos: 75,
+      Matriculados: 65,
+      Graduados: 55,
+    },
+    {
+      Código: "10902",
+      Programa: "Diseño gráfico",
+      Periodo: "2023",
+      Inscritos: 90,
+      Admitidos: 75,
+      Matriculados: 65,
+      Graduados: 55,
+    },
+    {
+      Código: "909092",
+      Programa: "Diseño de modas",
+      Periodo: "2023",
+      Inscritos: 90,
+      Admitidos: 75,
+      Matriculados: 65,
+      Graduados: 55,
+    },
+    {
+      Código: "909092",
+      Programa: "Diseño de modas",
+      Periodo: "2023",
+      Inscritos: 90,
+      Admitidos: 75,
+      Matriculados: 65,
+      Graduados: 55,
+    },
+    {
+      Código: "909092",
+      Programa: "Diseño de modas",
+      Periodo: "2023",
+      Inscritos: 90,
+      Admitidos: 75,
+      Matriculados: 65,
+      Graduados: 55,
+    },
+    {
+      Código: "909092",
+      Programa: "Diseño de modas",
+      Periodo: "2023",
+      Inscritos: 90,
+      Admitidos: 75,
+      Matriculados: 65,
+      Graduados: 55,
+    },
+    {
+      Código: "898978",
+      Programa: "Hoteleria y turismo",
+      Periodo: "2023",
+      Inscritos: 90,
+      Admitidos: 75,
+      Matriculados: 65,
+      Graduados: 55,
+    },
+    {
+      Código: "898978",
+      Programa: "Hoteleria y turismo",
+      Periodo: "2023",
+      Inscritos: 90,
+      Admitidos: 75,
+      Matriculados: 65,
+      Graduados: 55,
+    },
+    {
+      Código: "898978",
+      Programa: "Hoteleria y turismo",
+      Periodo: "2023",
+      Inscritos: 90,
+      Admitidos: 75,
+      Matriculados: 65,
+      Graduados: 55,
+    },
+    {
+      Código: "898978",
+      Programa: "Hoteleria y turismo",
+      Periodo: "2023",
+      Inscritos: 90,
+      Admitidos: 75,
+      Matriculados: 65,
+      Graduados: 55,
+    },
+    {
+      Código: "656745",
+      Programa: "Negocios internacionales",
+      Periodo: "2023",
+      Inscritos: 90,
+      Admitidos: 75,
+      Matriculados: 65,
+      Graduados: 55,
+    }
+  ];
+
   const tableClass = estadoSideBar ? styles.tableOpen : styles.tableClose;
   const thClass = estadoSideBar ? styles.thOpen : styles.thClose;
   const inputContainerClass = estadoSideBar
-    ? styles.inputContainerOpen
-    : styles.inputContainerClose;
+  ? styles.inputContainerOpen
+  : styles.inputContainerClose;
+
+  const paginationClass = estadoSideBar ? styles.paginationOpen : styles.paginationClose;
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentData = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(data.length / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
 
   return (
     <>
@@ -36,53 +180,28 @@ export const StatisticalInformationComponent = ({ estadoSideBar }) => {
           </tr>
         </thead>
         <tbody className={styles["table-hover"]}>
-          <tr>
-            <td className={styles["text-left"]}>January</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-          </tr>
-          <tr>
-            <td className={styles["text-left"]}>February</td>
-            <td className={styles["text-left"]}>$ 10,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-          </tr>
-          <tr>
-            <td className={styles["text-left"]}>March</td>
-            <td className={styles["text-left"]}>$ 85,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-          </tr>
-          <tr>
-            <td className={styles["text-left"]}>April</td>
-            <td className={styles["text-left"]}>$ 56,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-          </tr>
-          <tr>
-            <td className={styles["text-left"]}>May</td>
-            <td className={styles["text-left"]}>$ 98,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-            <td className={styles["text-left"]}>$ 50,000.00</td>
-          </tr>
+          {currentData.map((item, index) => (
+            <tr key={index}>
+              {Object.values(item).map((value, i) => (
+                <td key={i} className={styles["text-left"]}>
+                  {value}
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
+      <div className={paginationClass}>
+        {pageNumbers.map((number) => (
+          <button
+            key={number}
+            onClick={() => setCurrentPage(number)}
+            className={number === currentPage ? "active" : ""}
+          >
+            {number}
+          </button>
+        ))}
+      </div>
     </>
   );
 };
